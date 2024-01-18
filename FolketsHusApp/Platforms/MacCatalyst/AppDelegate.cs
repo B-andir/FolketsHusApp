c28e306b5f76b17f11654660ddf0bc42dc1b10a1
Daniel Svenberg <svenberg.daniel@gmail.com>
2024-01-18 22:22:49 +0100

    Initial Setup

---
+++ FolketsHusApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace FolketsHusApp {
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
