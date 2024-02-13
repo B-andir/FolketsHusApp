a64a470bc88f10a3c04cb1a89bcfac9d1a74cd43
Daniel Svenberg <svenberg.daniel@gmail.com>
2024-02-13 12:22:41 +0100

    feat: Dynamic quick add page, and custom splash screen

--- FolketsHusApp/Pages/BioRosenQuickPage.xaml.cs
+++ FolketsHusApp/Pages/BioRosenQuickPage.xaml.cs
@@ -1,4 +1,5 @@
 using FolketsHusApp.ViewModel;
+using System.Diagnostics;
 
 namespace FolketsHusApp.Pages;
 
@@ -6,5 +7,48 @@ public partial class BioRosenQuickPage : ContentPage {
     public BioRosenQuickPage(BioRosenQuickViewModel vm) {
         InitializeComponent();
         BindingContext = vm;
+    }
+
+    private void film_type_first_picker_SelectedIndexChanged(object sender, EventArgs e) {
+        start_vertical_stack_layout.IsVisible = false;
+        content_grid.IsVisible = true;
+    }
+
+    private void film_type_second_picker_SelectedIndexChanged(object sender, EventArgs e) {
+
+        // Reset all possibly changed values
+
+        live_time_and_pauses_container.IsVisible = false;
+        normal_film_length_label.IsVisible = true;
+        normal_film_length_time_picker.IsVisible = true;
+        age_rating_label.IsVisible = true;
+        age_rating_picker.IsVisible = true;
+        ticket_url_label.IsVisible = true;
+        ticket_url_entry.IsVisible = true;
+        premiere_label.IsVisible = true;
+        premiere_check_box.IsVisible = true;
+
+
+        if (film_type_second_picker.SelectedIndex == 1) {
+            // Live på Bio
+
+            live_time_and_pauses_container.IsVisible = true;
+            normal_film_length_label.IsVisible = false;
+            normal_film_length_time_picker.IsVisible = false;
+
+            age_rating_label.IsVisible = false;
+            age_rating_picker.IsVisible = false;
+
+            premiere_label.IsVisible = false;
+            premiere_check_box.IsVisible = false;
+
+        } else if (film_type_second_picker.SelectedIndex == 2) {
+            // Bio Kontrast
+
+            ticket_url_label.IsVisible = false;
+            ticket_url_entry.IsVisible = false;
+
+        }
+
     }
 }
--- FolketsHusApp/ViewModel/BioRosenQuickViewModel.cs
+++ FolketsHusApp/ViewModel/BioRosenQuickViewModel.cs
@@ -1,10 +1,98 @@
-﻿
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FolketsHusApp.Models;
+using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Diagnostics;
 
-namespace FolketsHusApp.ViewModel {
-    public partial class BioRosenQuickViewModel : ObservableObject {
+namespace FolketsHusApp.ViewModel;
+
+internal class SaveShowParams {
+    [JsonProperty("title")]
+    public string Title { get; set; } = "";
+
+    [JsonProperty("description")]
+    public string Description { get; set; } = "";
+
+    [JsonProperty("posterSource")]
+    public string PosterSource { get; set; } = "";
+
+    [JsonProperty("trailerURL")]
+    public string TrailerURL { get; set; } = "";
+
+    [JsonProperty("ticketURL")]
+    public string TicketURL { get; set; } = "";
+
+    [JsonProperty("ageRating")]
+    public string AgeRating { get; set; } = "";
+
+    [JsonProperty("genre")]
+    public string Genre { get; set; } = "";
+
+    [JsonProperty("runTime")]
+    public int RunTime { get; set; }
+
+    [JsonProperty("livePauses")]
+    public int LivePauses { get; set; } = 0;
+
+    [JsonProperty("isPremiere")]
+    public bool IsPremiere { get; set; }
+
+    [JsonProperty("runDate")]
+    public DateTime RunDate { get; set; }
+
+}
+
+public partial class BioRosenQuickViewModel : ObservableObject {
+
+    [ObservableProperty]
+    int filmTypePickerIndex = -1, ageRatingIndex = 0, livePausesCount = 0;
+
+    [ObservableProperty]
+    string? filmTitle, description, trailerURL, ticketURL, posterSource;
+
+    [ObservableProperty]
+    bool isPremiere = false, posterImageVisible = false, loading = false;
+
+    [ObservableProperty]
+    TimeSpan? runTime, runDateTime;
+
+    [ObservableProperty]
+    DateTime? runDate;
+
+    [ObservableProperty]
+    public SelectAgeRatingItem? ageRating;
+
+    [ObservableProperty]
+    public List<SelectAgeRatingItem> ageRatingItems = new() {
+        new SelectAgeRatingItem("Ingen Åldersgräns", ""),
+        new SelectAgeRatingItem("7 år", "7 år"),
+        new SelectAgeRatingItem("11 år", "11 år"),
+        new SelectAgeRatingItem("15 år", "15 år")
+    };
+
+    [ObservableProperty]
+    public List<string> genresItems = new List<string>() { "Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy", "Horror", "Musical", "Mystery", "Romance", "Science Fiction", "Thriller", "War", "Western" };
+
+    [ObservableProperty]
+    public List<int> genresIndices = new List<int>();
 
 
+    [RelayCommand]
+    async Task ImageSelect() {
+        if (MediaPicker.Default.IsCaptureSupported) {
+            FileResult photo = await MediaPicker.Default.PickPhotoAsync();
 
+            if (photo != null) {
+                PosterImageVisible = true;
+                PosterSource = photo.FullPath;
+            }
+        }
+    }
+
+
+    [RelayCommand]
+    async Task SaveShow() {
+        Debug.WriteLine("Saving is not yet implemented!");
     }
 }
