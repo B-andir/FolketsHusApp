0b944c8a5fd414d8fa3bfd1cf0364addfbd01f85
Daniel Svenberg <svenberg.daniel@gmail.com>
2024-02-15 00:33:32 +0100

    tweak: Switched API url from Localhost to Public IP

--- FolketsHusApp/APIService.cs
+++ FolketsHusApp/APIService.cs
@@ -37,8 +37,8 @@ public class APIService : IAPIService {
 
     // IMPORTANT! Change "http" to "https" when deployed and SSL Certificate is created.
     // Change Url to webdomain when ready for production.
-    //const string url = "http://146.190.16.245:80/api";  // Public IP
-    const string url = "http://10.0.2.2:5100/api";  // Localhost
+    const string url = "http://146.190.16.245:80/api";  // Public IP
+    //const string url = "http://10.0.2.2:5100/api";  // Localhost
 
     private INavigationService navigationService;
     private IConnectivity connectivity;
